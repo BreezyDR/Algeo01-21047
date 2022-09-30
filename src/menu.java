6908908be79f51ae158cb54c20ffdb19bc6ae43e
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-09-30 17:45:53 +0700

    change inputKeyboard

--- src/menu.java
+++ src/menu.java
@@ -20,4 +20,40 @@ public class menu extends matrixmethods {
         }
         return input;
     }
+
+    private static double[][] inputKeyboard(boolean harusSquare) {
+        double[][] matrix;
+        int row = 0;
+        int col = 0;
+        int i, j;
+        boolean terusinput = true;
+        while (terusinput) {
+            if (harusSquare) {
+                printlnstr("Silahkan masukkan nilai n dalam dimensi matriks n x n");
+                print("--> ");
+                row = scanner.nextInt();
+                col = row;
+            } else {
+                printlnstr("Silahkan masukkan nilai m dan n dalam dimensi matriks m x n.");
+                print("-->");
+                row = scanner.nextInt();
+                col = scanner.nextInt();
+            }
+            if (row > 0 && col > 0) {
+                terusinput = false;
+            } else {
+                printlnstr("Nilai invalid! Silahkan coba lagi.");
+            }
+        }
+        matrix = new double [row][col];
+
+        printlnstr("Masukkan elemen matriks:");
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                print("Baris ke-" + (i+1) + " kolom ke-" + (j+1) + ": ");
+                matrix[i][j] = scanner.nextDouble();
+            }
+        }
+        return matrix;
+    }
 }
