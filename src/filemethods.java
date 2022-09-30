e3878293a464cbfda9f8007667f6075a412919b4
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2022-09-30 22:35:37 +0700

    Menambahkan pada filemethods.java
    writeMatrixDeterminankeFile, writeMatrixInversekeFile,
    writeMatrixkeFile, banyakRowandCol

--- src/filemethods.java
+++ src/filemethods.java
@@ -1,9 +1,11 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class filemethods extends matrixmethods {
-    public static double[][] bacaMatriksFile(int row, int col, String file) {
+    public static double[][] readMatrixFile(int row, int col, String file) {
         double[][] matrix = new double[row][col];
 
         FileReader reader = null;
@@ -25,4 +27,117 @@ public class filemethods extends matrixmethods {
         scanmatrix.close();
         return matrix;
     }
+
+    public static int[] banyakRowandCol(String file) {
+        FileReader reader = null;
+        try {
+            reader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            // TODO: handle exception
+            printlnstr("File tidak dapat ditemukan! Silakan coba lagi.");
+        }
+
+        int row = 0;
+        int col = 0;
+        String stringrow = "";
+        Scanner rowscanner = new Scanner(reader);
+        while (rowscanner.hasNextLine()) {
+            row++;
+            stringrow = rowscanner.nextLine();
+        }
+        Scanner colscanner = new Scanner(stringrow);
+        while (colscanner.hasNextDouble()) {
+            col++;
+            colscanner.nextDouble();
+        }
+        rowscanner.close();
+        colscanner.close();
+
+        int[] rowandcol = new int[2];
+        rowandcol[0] = row;
+        rowandcol[1] = col;
+        return rowandcol;        
+    }
+
+    public static boolean writeMatrixkeFile(String directory, double[][] matrix) {
+        try {
+            FileWriter writer = new FileWriter(directory);
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    writer.write(Double.toString(matrix[i][j]) + " ");
+                }
+                writer.write("\n");
+            }
+            writer.write("\n");
+            writer.close();
+            printlnstr("Hasil sudah dituliskan kepada file.");
+            return true;
+        } catch (IOException e) {
+            // TODO: handle exception
+            printlnstr("Something went wrong. Try again");
+            return false;
+        }
+    }
+
+    public static boolean writeMatrixDeterminankeFile(String directory, double[][] matrix, double det) {
+        try {
+            FileWriter writer = new FileWriter(directory);
+            writer.write("Determinan matriks berikut \n");
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    writer.write(Double.toString(matrix[i][j]) + " ");
+                }
+                writer.write("\n");
+            }
+            writer.write("\nadalah");
+            writer.write(Double.toString(det));
+            writer.write("\n");
+            writer.close();
+            printlnstr("Hasil sudah dituliskan kepada file.");
+            return true;
+        } catch (IOException e) {
+            // TODO: handle exception
+            printlnstr("Something went wrong. Try again");
+            return false;
+        }
+    }
+
+    public static boolean writeMatrixInversekeFile(String directory, double[][] matrix, double[][] inverse, boolean hasInverse) {
+        try {
+            FileWriter writer = new FileWriter(directory);
+            if (hasInverse) {
+                writer.write("Invers matriks berikut \n");
+                for (int i = 0; i < matrix.length; i++) {
+                    for (int j = 0; j < matrix[i].length; j++) {
+                        writer.write(Double.toString(matrix[i][j]) + " ");
+                    }
+                    writer.write("\n");
+                }
+                writer.write("\nadalah");
+                for (int i = 0; i < inverse.length; i++) {
+                    for (int j = 0; j < inverse[i].length; j++) {
+                        writer.write(Double.toString(inverse[i][j]) + " ");
+                    }
+                    writer.write("\n");
+                }
+            } else {
+                writer.write("Matriks berikut \n");
+                for (int i = 0; i < matrix.length; i++) {
+                    for (int j = 0; j < matrix[i].length; j++) {
+                        writer.write(Double.toString(matrix[i][j]) + " ");
+                    }
+                    writer.write("\n");
+                }
+                writer.write("\nTidak memiliki invers.");
+            }
+            writer.write("\n");
+            writer.close();
+            printlnstr("Hasil sudah dituliskan kepada file.");
+            return true;
+        } catch (IOException e) {
+            // TODO: handle exception
+            printlnstr("Something went wrong. Try again");
+            return false;
+        }
+    }    
 }
